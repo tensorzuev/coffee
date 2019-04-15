e9ae91aa594e6425ac6bdd89693987e1c0694e23
Dmitriy <xakkez@gmail.com>
2019-04-15 23:40:42 +0300

    index page

--- src/Cabinet/BodyRouter.tsx
+++ src/Cabinet/BodyRouter.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import { Machines } from './Machines';
 import { Detail } from './Detail/Detail';
 
 export function BodyRouter() {
     return (
         <main aria-live='polite'>
+            <Route exact path='/' render={ () => 
+                        <div>
+                            Разводящая страница в разработке.
+                            <br />
+                            Перейдите к <NavLink to='/main/list'>списку</NavLink> или сразу к <NavLink to='/detail/0'>информации о кофе-машине</NavLink>
+                        </div>} />
             <Route path='/main/list' component={Machines} />
             <Route path='/main/appeal' render={() => <div>APPEaK</div>} />
 
